d2998baa2a63f5fbab76e26db094c6621e4d6630
lllytnik <65924940+lllytnik@users.noreply.github.com>
2023-04-01 22:00:08 +0400

    51 lesson

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import s from "./Users.module.css";
 import usersPic from "../../images/dialog/user1.png";
+import axios from 'axios';
 let Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-            { id: 1, photoUrl: usersPic, followed: false, fullName: 'Dmitry', status: 'I am a boss', location: { city: 'Minsk', country: 'Belarus' } },
-            { id: 2, photoUrl: usersPic, followed: true, fullName: 'Sacha', status: 'I am a boss two', location: { city: 'Moscow', country: 'Russia' } }
-        ]
-        )
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items);
+        });
     }
 
     return <div className={s.users}>
@@ -15,13 +14,13 @@ let Users = (props) => {
             props.users.map(u => <div className={s.user} key={u.id}>
                 <div className={s.wrapper}>
                     <div className={s.avatar}>
-                        <img src={u.photoUrl} alt="avatar" />
+                        <img src={u.photos.small != null ? u.photos.small : usersPic} alt="avatar" />
                     </div>
-                    <div className={s.name}>{u.fullName}</div>
+                    <div className={s.name}>{u.name}</div>
                     <div className={s.status}>{u.status}</div>
                     <div className={s.location}>
-                        <div className={s.locationCountry}>{u.location.country}:&nbsp;</div>
-                        <div className={s.locationCity}>{u.location.city}</div>
+                        <div className={s.locationCountry}>{"u.location.country"}:&nbsp;</div>
+                        <div className={s.locationCity}>{"u.location.city"}</div>
                     </div>
                 </div>
 
