2e21c57a98ab4eee874cb35dc912df0045d420d8
lllytnik <65924940+lllytnik@users.noreply.github.com>
2023-04-08 23:46:11 +0400

    53 lesson

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -2,37 +2,41 @@ import React from "react";
 import s from "./Users.module.css";
 import usersPic from "../../images/dialog/user1.png";
 import axios from 'axios';
-let Users = (props) => {
-    if (props.users.length === 0) {
+
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
+            this.props.setUsers(response.data.items);
         });
     }
-
-    return <div className={s.users}>
-        {
-            props.users.map(u => <div className={s.user} key={u.id}>
-                <div className={s.wrapper}>
-                    <div className={s.avatar}>
-                        <img src={u.photos.small != null ? u.photos.small : usersPic} alt="avatar" />
+    render() {
+        return <div className={s.users}>
+            {
+                this.props.users.map(u => <div className={s.user} key={u.id}>
+                    <div className={s.wrapper}>
+                        <div className={s.avatar}>
+                            <img src={u.photos.small != null ? u.photos.small : usersPic} alt="avatar" />
+                        </div>
+                        <div className={s.name}>{u.name}</div>
+                        <div className={s.status}>{u.status}</div>
+                        <div className={s.location}>
+                            <div className={s.locationCountry}>{"u.location.country"}:&nbsp;</div>
+                            <div className={s.locationCity}>{"u.location.city"}</div>
+                        </div>
                     </div>
-                    <div className={s.name}>{u.name}</div>
-                    <div className={s.status}>{u.status}</div>
-                    <div className={s.location}>
-                        <div className={s.locationCountry}>{"u.location.country"}:&nbsp;</div>
-                        <div className={s.locationCity}>{"u.location.city"}</div>
+
+                    <div className={s.wrapBtn}>
+                        {u.followed
+                            ? <button className={s.btnUnfollow} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                            : <button className={s.btnFollow} onClick={() => { this.props.follow(u.id) }}>Follow</button>}
                     </div>
-                </div>
-
-                <div className={s.wrapBtn}>
-                    {u.followed
-                        ? <button className={s.btnUnfollow} onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                        : <button className={s.btnFollow} onClick={() => { props.follow(u.id) }}>Follow</button>}
-                </div>
 
 
-            </div>)
-        }
-    </div >
+                </div>)
+            }
+        </div >
+    }
 }
+
 export default Users;
